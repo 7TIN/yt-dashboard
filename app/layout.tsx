e4d5fb75274cbd0864bb16f7b54ba1808edd41d8
7TIN <prasadgujarathi619@gmail.com>
2025-08-17 14:00:26 +0530

    fix: layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProvider from "@/components/SessionProvider";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
